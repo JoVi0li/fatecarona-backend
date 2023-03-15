e1880780ed4c7f79a27b05c862078a0193edfe06
João Vitor <jovioli.dev04@gmail.com>
2023-03-14 23:33:19 -0300

    Feat(App): Add swagger

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,8 @@ import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import dotenv from "dotenv";
 import fjwt from "@fastify/jwt";
 import multipart from "@fastify/multipart";
+import swagger from "@fastify/swagger";
+import swaggerIo from "@fastify/swagger-ui";
 import { userRoutes } from "../modules/user";
 import { collegeRoutes } from "../modules/college";
 import { courseRoutes } from "../modules/course";
@@ -22,6 +24,30 @@ const buildServer = async () => {
   server.register(fjwt, { secret: secret });
 
   server.register(multipart);
+
+  server.register(swagger, {
+    swagger: {
+      info: {
+        version: "1.0",
+        title: "Fatecarona",
+        description: "API Documentation",
+        contact: {
+          name: "João Vitor Oliveira",
+          url: "https://github.com/JoVi0li",
+          email: "jovi.oli04@gmail.com"
+        },
+        license: {
+          name: "MIT"
+        },
+      },
+      schemes: ["http", "https"],
+      consumes: ["application/json", "multipart/form-data"],
+      produces: ["application/json"]
+
+    }
+  });
+
+  server.register(swaggerIo, { routePrefix: '/docs', });
 
   server.decorate(
     "auth",
@@ -62,9 +88,9 @@ const buildServer = async () => {
   server.register(authRoutes, { prefix: 'api/auth' });
 
   try {
-    await server.listen({
-      port: port,
-    });
+    await server.ready();
+    await server.listen({ port: port });
+    server.swagger();
     console.log(`Running on: ${port}`)
   } catch (error) {
     console.error(error);
